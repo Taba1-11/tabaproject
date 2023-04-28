86238e9c9f54b86dec5963a5c8f462f746d0588d
Taba1-11 <130458225+Taba1-11@users.noreply.github.com>
2023-04-28 05:15:53 -0800

    Add files via upload

---
+++ Project.js
@@ -0,0 +1,28 @@
+
+
+
+document.querySelector('.dropbtn').addEventListener('click', function() {
+  var dropdownContent = document.querySelector('.dropdown-content');
+  if (dropdownContent.style.display === 'block') {
+    dropdownContent.style.display = 'none';
+  } else {
+    dropdownContent.style.display = 'block';
+  }
+});
+
+document.querySelector('.dropbtn2').addEventListener('click', function() {
+  var dropdownContent = document.querySelector('.dropdown-content2');
+  if (dropdownContent.style.display === 'block') {
+    dropdownContent.style.display = 'none';
+  } else {
+    dropdownContent.style.display = 'block';
+  }
+});
+document.querySelector('.dropbtn3').addEventListener('click', function() {
+  var dropdownContent = document.querySelector('.dropdown-content3');
+  if (dropdownContent.style.display === 'block') {
+    dropdownContent.style.display = 'none';
+  } else {
+    dropdownContent.style.display = 'block';
+  }
+});
